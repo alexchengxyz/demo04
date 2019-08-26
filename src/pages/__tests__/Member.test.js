1b0b29a9b71e7e939c5b048ec96109773f28c2c4
Alex10_Chen <alex10_chen@bb.com>
2019-08-26 19:33:06 +0800

    jest axios test v3

--- src/pages/__tests__/Member.test.js
+++ src/pages/__tests__/Member.test.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16'
-import '@testing-library/jest-dom/extend-expect';
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import axios from 'axios';
 import Member from '../Member';
-
-jest.mock('axios');
 configure({ adapter: new Adapter() });
+jest.mock('axios');
 
 
-it('renders correctly', () => {
+it('test axios', () => {
+  const getSpy = jest.spyOn(axios, 'get');
+  const wrapper = shallow( <Member /> );
 
-  const users = [
-    { id: 1, username: "user1", enable: 0, locked: 0, created_at: "2019-08-17T06:42:58+00:00" },
-    { id: 2, username: "user2", enable: 0, locked: 0, created_at: "2019-08-17T06:42:58+00:00" },
-  ];
-
-  const page = {
-    first_result: "0",
-    max_results: "20",
-    total: 2
-  }
-
-  const resp = {result: "ok",ret: users, pagination: page};
-
-  axios.get.mockResolvedValue(resp);
-
-  // console.log(resp);
-
-  const tree = renderer.create(<Member />).toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(getSpy).toBeCalled();
+  //expect(wrapper).toMatchSnapshot();
 });
